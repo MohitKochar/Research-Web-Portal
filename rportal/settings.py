1291953cfcdccf66440d7bc23fafac7ac9cc24e6
Mohit <mohitkochar0203@gmail.com>
2019-08-06 22:07:07 +0530

    Revert "Update settings.py"
    This reverts commit bad513e04c52d4f977d6a4b4abf9bedf473d5f78.

--- rportal/settings.py
+++ rportal/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.0/ref/settings/
 """
-
+import django_heroku
 import os
 from django.contrib.messages import constants as messages
 
@@ -154,3 +154,4 @@ MESSAGE_TAGS = {
 
 
 # Activate Django-Heroku.
+django_heroku.settings(locals())
